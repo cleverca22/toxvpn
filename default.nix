93e50274cc9821cec7f18064db62a29ee17d75d7
Remy Goldschmidt <taktoa@gmail.com>
2017-02-28 02:49:08 -0600

    Fixed release.nix to work with Hydra

---
+++ default.nix
@@ -0,0 +1,45 @@
+{ stdenv, clangStdenv, lib, fetchFromGitHub
+, cmake, libsodium, systemd, nlohmann_json, libtoxcore, libcap, zeromq
+}:
+
+with rec {
+  enableDebugging = true;
+
+  libtoxcoreLocked = stdenv.lib.overrideDerivation libtoxcore (old: {
+    name = "libtoxcore-20160907";
+
+    src = fetchFromGitHub {
+      owner  = "TokTok";
+      repo   = "c-toxcore";
+      rev    = "1387c8f15032cab1af1c6444621b62af8d3a5494";
+      sha256 = "1wd5l56fb1lrrjxsgnzr6199kiw81jipyr8f395gbka4bzysilzq";
+    };
+
+    dontStrip = enableDebugging;
+  });
+};
+
+stdenv.mkDerivation {
+  name = "toxvpn-git";
+
+  src = ./.;
+
+  dontStrip = enableDebugging;
+
+  NIX_CFLAGS_COMPILE = if enableDebugging then [ "-ggdb -Og" ] else [];
+
+  buildInputs = lib.concatLists [
+    [ cmake libtoxcoreLocked nlohmann_json libsodium libcap zeromq ]
+    (lib.optional (systemd != null) systemd)
+  ];
+
+  cmakeFlags = (lib.optional (systemd != null) [ "-DSYSTEMD=1" ]);
+
+  meta = with stdenv.lib; {
+    description = "A tool for making tunneled connections over Tox";
+    homepage    = "https://github.com/cleverca22/toxvpn";
+    license     = licenses.gpl3;
+    maintainers = with maintainers; [ cleverca22 obadz ];
+    platforms   = platforms.linux;
+  };
+}
